0074d124ee2f2d3f0a28074be95bc2fa03216703
Alec van der Doef <68061911+Demwan@users.noreply.github.com>
2025-01-19 00:29:36 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
-$host = "janc-au-su-jancausu.d.aivencloud.com";
-$username = "avnadmin";
-$password = "AVNS_OEvCMARrGprxYNdVy94";
-$dbname = "defaultdb";
+$host = "";
+$username = "";
+$password = "";
+$dbname = "";
 ?>
